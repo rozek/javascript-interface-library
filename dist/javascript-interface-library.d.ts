1c5fb59308ee4002158c58ed5b6b51341c0a4487
Andreas Rozek <A.Rozek@gmx.de>
2021-06-21 15:53:30 +0200

    new build

--- dist/javascript-interface-library.d.ts
+++ dist/javascript-interface-library.d.ts
@@ -246,6 +246,8 @@ export declare function ValuesDiffer(thisValue: any, otherValue: any): boolean;
 export declare function ValuesAreEqual(thisValue: any, otherValue: any): boolean;
 /**** ObjectIsEmpty ****/
 export declare function ObjectIsEmpty(Candidate: any): boolean;
+/**** ObjectIsNotEmpty ****/
+export declare function ObjectIsNotEmpty(Candidate: string): boolean;
 /**** StringIsEmpty ****/
 export declare function StringIsEmpty(Candidate: string): boolean;
 /**** StringIsNotEmpty ****/
